76d5ef552a047515abeb7e8590c7c8643eb9a046
CrackNicolas <beltrannicolasalejo@gmail.com>
2023-03-08 15:05:56 -0300

    My youtube v1.6

--- src/components/partials/nav_top.js
+++ src/components/partials/nav_top.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+import ComponentNavToggler from "./nav_toggler.js";
+
 import Schema_styles_nav_top from '../../styles/schema/styles_nav_top.js';
 
 export default function ComponentNavTop({search_query}){
@@ -13,123 +15,15 @@ export default function ComponentNavTop({search_query}){
 
     return (
         <nav className="nav-top">
-            <button className="menu" onClick={() => view_menu()}>
-            <ion-icon name="menu-outline"></ion-icon>
-            <div className="items-menu" style={styles}>
-                <div className="items">
-                    <div className="item">
-                        <ion-icon name="home"></ion-icon>
-                        <a href="/">Inicio</a>
-                    </div>
-                    <div className="item">
-                        <ion-icon name="triangle-outline"></ion-icon>
-                        <a>Shorts</a>
-                    </div>
-                    <div className="item">
-                        <ion-icon name="copy-outline"></ion-icon>
-                        <a>Supscripciones</a>
-                    </div>
-                    <div className="linea-item">__________________________</div>
-                    <div className="item">
-                        <ion-icon name="library-outline"></ion-icon>
-                        <a>Mi biblioteca</a>
-                    </div>
-                    <div className="item">
-                        <ion-icon name="stopwatch-outline"></ion-icon>
-                        <a>Historial</a>
-                    </div>
-                    <div className="item">
-                        <ion-icon name="caret-forward-circle-outline"></ion-icon>
-                        <a>Mis videos</a>
-                    </div>
-                    <div className="item">
-                        <ion-icon name="alarm-outline"></ion-icon>
-                        <a>Ver mas tarde</a>
-                    </div>
-                    <div className="item">
-                        <ion-icon name="thumbs-up-outline"></ion-icon>
-                        <a>Videos que me gustan</a>
-                    </div>
-                    <div className="item">
-                        <ion-icon name="play-circle-outline"></ion-icon>
-                        <a>Videos importantes</a>
-                    </div>
-                    <div className="linea-item">__________________________</div>
-                    <p className="sub-title-item">Suscripciones</p>
-                    <div className="item item-subscription">
-                        <img src="/images/my_perfil.jpg" alt=""/>
-                        <a>Canales</a>
-                    </div>
-                    <div className="item item-subscription">
-                        <img src="/images/my_perfil.jpg" alt=""/>
-                        <a>Canales</a>
-                    </div>
-                    <div className="linea-item">__________________________</div>
-                    <p className="sub-title-item">Explorar</p>
-                    <div className="item">
-                        <ion-icon name="bonfire-outline"></ion-icon>
-                        <a>Tendencias</a>
-                    </div>
-                    <div className="item">
-                        <ion-icon name="musical-note-outline"></ion-icon>
-                        <a>Musica</a>
-                    </div>
-                    <div className="item">
-                        <ion-icon name="film-outline"></ion-icon>
-                        <a>Peliculas</a>
-                    </div>
-                    <div className="item">
-                        <ion-icon name="radio-outline"></ion-icon>
-                        <a>En directo</a>
-                    </div>
-                    <div className="item">
-                        <ion-icon name="game-controller-outline"></ion-icon>
-                        <a>Videojuegos</a>
-                    </div>
-                    <div className="item">
-                        <ion-icon name="newspaper-outline"></ion-icon>
-                        <a>Noticias</a>
-                    </div>
-                    <div className="item">
-                        <ion-icon name="trophy-outline"></ion-icon>
-                        <a>Deportes</a>
-                    </div>
-                    <div className="item">
-                        <ion-icon name="bulb-outline"></ion-icon>
-                        <a>Aprendizaje</a>
-                    </div>
-                    <div className="linea-item">__________________________</div>
-                    <div className="item">
-                        <ion-icon name="settings-outline"></ion-icon>
-                        <a>Configuracion</a>
-                    </div>
-                    <div className="item">
-                        <ion-icon name="flag-outline"></ion-icon>
-                        <a>Historial de denuncias</a>
-                    </div>
-                    <div className="item">
-                        <ion-icon name="help-circle-outline"></ion-icon>
-                        <a>Ayuda</a>
-                    </div>
-                    <div className="item">
-                        <ion-icon name="chatbubble-ellipses-outline"></ion-icon>
-                        <a>Enviar sugerencias</a>
-                    </div>
-                    <div className="linea-item">__________________________</div>
-                    <p className="sub-title-footer-item">Informacion Prensa Derechos de autor Contactar Creadores Publicidad Desarrolladores</p>
-                    <p className="sub-title-footer-item">Terminos Privacidad Politica y seguridad Como funciona YouTube Probar funciones nuevas</p>
-                    <p className="item-footer">
-                        <ion-icon name="reload-circle-outline"></ion-icon>
-                        2023 Google LLC
-                    </p>
-                </div>
-            </div>
-            </button>
             <div className="logo">
-                <a className="content-logo" href="/" title="Página de inicio de YouTube">
-                    <ion-icon name="logo-youtube" title="Página de inicio de YouTube"></ion-icon>
+                <div className="menu" onClick={() => view_menu()}>
+                    <ion-icon name="menu-outline"></ion-icon>
+                    <ComponentNavToggler styles={styles}/>
+                </div>
+                <a href="/">
+                    <img src="/images/youtube.png"/>
+                    <p>YouTube</p>
                 </a>
-                <a href="/" title="Página de inicio de YouTube">YouTube</a>
             </div>
             <form action="/results" method="GET" className="search" title="Buscar">
                 <input type="text" name="search_query" defaultValue={search_query} placeholder="Buscar"/>
