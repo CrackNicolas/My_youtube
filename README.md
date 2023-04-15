1db238901b92bca979b58e19381732ec1eabc9ab
CrackNicolas <beltrannicolasalejo@gmail.com>
2023-04-15 13:06:17 -0300

    My youtube v3.5

--- README.md
+++ README.md
@@ -1,16 +1,3 @@
-/**---------Errores pendientes detectados solucionar------------**/
-    1 - Evitar que las categorias de los videos related se repitan
-    2 - Evitar que se muestre el mostrar mas si no hay contenido para mostrar
-    3 - Hacer que se oculten las opciones de los videos home cuando hago doble click
-
-    ---------------------------------------------------------------------- 
-    --  No olvidar hacer reduccion una vez que cada funcionalidad este  --
-    --             completa por completo sin excepciones                --
-    ----------------------------------------------------------------------
-
-/**-------------------------------------------------------------**/
-
-
 #Project name --> My youtube
 
 ** Instalacion
--- src/service/config/credentials.js
+++ src/service/config/credentials.js
@@ -1,8 +1,3 @@
-//**BORRAR ANTES DE HACER EL DEPLOY**//
-//**Id unico de mi canal --->  UCctJ1hstsPfgfqXPlRsH7QQ   **//
-//**----Volver a colocar mi lista de suscripciones como privada desde el youtube oficial-----**/
-
-
 //Cuenta Nicolas
 /*
 export const key = "AIzaSyDgJcdDk41JxkJZvmPfPgl5E8fdbzuZOm4";
@@ -10,20 +5,11 @@ export const url = "https://youtube.googleapis.com/youtube/v3/";
 */
 //Cuenta alejo
 
-
 export const key = "AIzaSyD5eKr99b_WyjUc8_Wh3DOyn-OiXBks4Sg";
 export const url = "https://youtube.googleapis.com/youtube/v3/";
-
 
 //Cuenta del otro Nicolas
 /*
 export const key = "AIzaSyC0WhgzDQO010Zy4igk7g1x2v0NyoRiOks";
 export const url = "https://youtube.googleapis.com/youtube/v3/";
-*/
-
-/**-----------Clave de google autocomplete------------------**/
-/*
-export const key = "AIzaSyDgJcdDk41JxkJZvmPfPgl5E8fdbzuZOm4";
-export const url = "https://customsearch.googleapis.com/customsearch/v1";
-export const cx = "b7aaa197f21544f90";  //https://cse.google.com/cse?cx=b7aaa197f21544f90
 */
