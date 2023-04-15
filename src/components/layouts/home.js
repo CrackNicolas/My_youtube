0b98f3aaa2c70442c0e2d240e9ecfa7b47c0867e
CrackNicolas <beltrannicolasalejo@gmail.com>
2023-04-15 09:41:58 -0300

    My youtube v3.5

--- src/components/layouts/home.js
+++ src/components/layouts/home.js
@@ -6,6 +6,9 @@ import ComponentNavLeft from '../partials/menus/left.js';
 import ComponentVideosLayouts from '../partials/videos/home.js';
 import ComponentSinInternet from './sin_internet.js';
 
+//<ComponentNavCategories categorie_selected={context_global.capture_id_categorie} categories={context_global.categories}/>
+//<ComponentVideosLayouts videos={context_global.videos}/>
+
 import {Global_context} from '../../context/global_context.js';
 export default function ComponentHome(){
   const context_global = useContext(Global_context);
@@ -14,9 +17,9 @@ export default function ComponentHome(){
     (context_global.internet)? 
       <React.Fragment>
         <ComponentNavTop search_query={context_global.search_query} user={context_global.user} style={{visibility : "visible"}}/>
-        <ComponentNavCategories categorie_selected={context_global.capture_id_categorie} categories={context_global.categories}/>
+        
         <ComponentNavLeft capture_icono_nav_left={context_global.capture_id_categorie} item_selected="0"/>
-        <ComponentVideosLayouts videos={context_global.videos}/>
+        
       </React.Fragment>
     :
       <ComponentSinInternet/>
