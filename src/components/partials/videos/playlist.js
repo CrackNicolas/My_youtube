d9664d258069c5c7bf363cbf34c99960001eb472
CrackNicolas <beltrannicolasalejo@gmail.com>
2023-06-13 16:20:45 -0300

    My youtube v4.2

--- src/components/partials/videos/playlist.js
+++ src/components/partials/videos/playlist.js
@@ -1,10 +1,28 @@
+import { useState } from 'react';
 import '../../../styles/partials/videos/playlist.css';
 
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+
+const reorder = (list, index_start, index_end) => {
+    const result = [...list];
+    const [removed] = result.splice(index_start, 1);
+    result.splice(index_end, 0, removed);
+    return result;
+};
+
+const handler = (result) => {
+    const { source, destination } = result;
+    if((!destination) || (source.index === destination.index && source.droppableId === destination.droppableId)) return ;
+    return ((prev) => reorder(prev, source.index, destination.index) );
+}
+
 export default function ComponentVideosPlaylist({channel,videos}){
+    const [videos_playlist, setVideos_playlist] = useState(videos);
+
     return (
         <section className='playlist'>
-            <article className='presentation' style={{backgroundImage: "url("+videos[0].url_imagen+")"}}>
-                <img src={videos[0].url_imagen}/>
+            <article className='presentation' style={{backgroundImage: "url("+videos_playlist[0].url_imagen+")"}}>
+                <img src={videos_playlist[0].url_imagen}/>
                 <div className='imagen'>
                     <div>
                         <ion-icon name="play"></ion-icon>
@@ -17,7 +35,7 @@ export default function ComponentVideosPlaylist({channel,videos}){
                         <span className='name'>{channel.title}</span>
                     </div>
                     <div>
-                        <span>6 videos</span>
+                        <span>{videos_playlist.length} {(videos_playlist.length==1)? "video" : "videos"}</span>
                         <span>0 visualizaciones</span>
                         <span>Actualizada hoy</span>
                     </div>
@@ -41,40 +59,51 @@ export default function ComponentVideosPlaylist({channel,videos}){
                     </div>
                 </div>
                 <div className='body'>
-                    <div className='items'>
-                        {
-                            videos.map((video,index) => {
-                                return (
-                                    <div className='item'>
-                                        <div className='icon-left'>
-                                            <ion-icon name="reorder-two-outline"></ion-icon>
-                                        </div>
-                                        <a href="" key={index} className='video'>
-                                            <div className="imagen">
-                                                <img src={video.url_imagen} alt={video.title}/>
-                                                <div className="duration">
-                                                    <span>
-                                                        {video.duration}
-                                                    </span>
-                                                </div>
-                                            </div>
-                                            <div className="description">
-                                                <p className="title">{video.title}</p>
-                                                <div className="data">
-                                                    <span title={video.channel.title}>{video.channel.title}</span>
-                                                    <span>. {video.view_count}</span>
-                                                    <span>. {video.time_elapsed}</span>
-                                                </div>
-                                            </div>
-                                        </a>
-                                        <div className='icon-right'>
-                                            <ion-icon name="ellipsis-vertical-outline"></ion-icon>
-                                        </div>
-                                    </div>
-                                )
-                            })
-                        }
-                    </div>
+                    <DragDropContext onDragEnd={(result) => setVideos_playlist(handler(result))}>
+                        <Droppable droppableId='items'>
+                            { (droppableProvided) => (
+                                <div className='items' {...droppableProvided.droppableProps} ref={droppableProvided.innerRef}>
+                                    {
+                                        videos_playlist.map((video,index) => {
+                                            return (
+                                                <Draggable key={index} draggableId={""+index} index={index}>
+                                                    { (draggableProvided) => ( 
+                                                        <div className='item' {...draggableProvided.draggableProps} ref={draggableProvided.innerRef}>
+                                                            <div {...draggableProvided.dragHandleProps} className='icon-left'>
+                                                                <ion-icon name="reorder-two-outline"></ion-icon>
+                                                            </div>
+                                                            <a href="" className='video'>
+                                                                <div className="imagen">
+                                                                    <img src={video.url_imagen} alt={video.title}/>
+                                                                    <div className="duration">
+                                                                        <span>
+                                                                            {video.duration}
+                                                                        </span>
+                                                                    </div>
+                                                                </div>
+                                                                <div className="description">
+                                                                    <p className="title">{video.title}</p>
+                                                                    <div className="data">
+                                                                        <span title={video.channel.title}>{video.channel.title}</span>
+                                                                        <span>. {video.view_count}</span>
+                                                                        <span>. {video.time_elapsed}</span>
+                                                                    </div>
+                                                                </div>
+                                                            </a>
+                                                            <div className='icon-right'>
+                                                                <ion-icon name="ellipsis-vertical-outline"></ion-icon>
+                                                            </div>
+                                                        </div>
+                                                    )}
+                                                </Draggable>   
+                                            )
+                                        })
+                                    }
+                                    {droppableProvided.placeholder}
+                                </div>
+                            )}
+                        </Droppable>
+                    </DragDropContext>
                 </div>
             </article>
         </section>
